ceb7220c077ba5f48aa758ed3231c11b7b23b018
rolf <rolf@l-plan.nl>
2020-05-01 16:35:34 +0200

    fix test with this version of rails and minitest

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,9 +1,38 @@
+# require_relative 'boot'
+
+
+# require "action_controller/railtie"
+# # require "action_mailer/railtie"
+# # require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Bundler.require(*Rails.groups)
+# require "app_version"
+
+# module Dummy
+#   class Application < Rails::Application
+#     # Settings in config/environments/* take precedence over those specified here.
+#     # Application configuration should go into files in config/initializers
+#     # -- all .rb files in that directory are automatically loaded.
+#   end
+# end
+
 require_relative 'boot'
 
+# require 'rails/all'
 
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+# require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
-# require "action_mailer/railtie"
-# require "sprockets/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
@@ -11,6 +40,9 @@ require "app_version"
 
 module Dummy
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
